7a28fa27d9fa1982c91a6dc8487430557ccbee73
Guy Drori <guydrori@users.noreply.github.com>
2019-06-10 01:41:15 +0200

    Adding Spring Security

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,4 +1,19 @@
 package app;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 public class Application {
+
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        .allowCredentials(true);
+//            }
+//        };
+//    }
 }
